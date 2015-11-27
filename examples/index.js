7aa6703410e3642b2d1ab710fe91344bc124716b
Marcus David Bronstein <marcusdb@gmail.com>
2015-11-27 18:59:46 -0200

    examples creations

--- examples/index.js
+++ examples/index.js
@@ -1,37 +1,15 @@
 var MicroGears=require('../src/index');
+var express = require('express');
+var app = express();
 
-var service = {
-    name: 'testService', path: "path",
-    testFunction1: function (arg1, arg2) {
-        console.log(this.name);
-        return true;
-    },
-    testFunction2: function (arg1, arg2) {
-        return arg1 + arg2;
-    },
-};
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-var plugin1 = function (chain, arg1, arg2) {
-    console.log(this.name);
-    var result = chain(arg1, arg2);
-
-    return result;
-};
-
-var plugin2 = function (chain, arg1, arg2) {
-    console.log(this.name);
-    var result = chain(arg1, arg2);
-
-    return result;
-};
-
-MicroGears.addPlugin(plugin1, 'test plugin');
-MicroGears.addPlugin(plugin2, 'test plugin2');
-
-MicroGears.addService(service);
-var plugin = function (chain, service, arg1, arg2) {
-    return chain(arg1, arg2);
-}
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
 
-MicroGears.testService.testFunction1({name: 'a'}, {});
+});
+
