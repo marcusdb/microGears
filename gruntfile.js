b2a6fe5cdb06a69d025e4bef3b050c9c3121a736
Diego Ramos <rdiego26@gmail.com>
2015-12-08 01:14:40 -0200

    Improve jshint and add watch task .

--- gruntfile.js
+++ gruntfile.js
@@ -1,44 +1,53 @@
 var grunt = require("grunt");
 
 grunt.initConfig({
-    jshint: {
-        all: ['src/**/*.js', '*.js', 'spec/**/*.js'],
-        options: {
-            undef: true,
-            node: true,
-            globals: {
-                require: true,
-                module: true,
-                setTimeout: true,
-                __dirname: true,
-                process: true,
-                setInterval: true,
-                /* MOCHA */
-                it: true,
-                describe: true,
-                before: true,
-                beforeEach: true,
-                after: true,
-                afterEach: true
-            }
-        }
-    },
-    mochaTest: {
-        test: {
-            options: {
-                reporter: 'spec',
-                //captureFile: 'results.txt', // Optionally capture the reporter output to a file
-                quiet: false, // Optionally suppress output to standard out (defaults to false)
-                clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
-            },
-            src: ['spec/**/*.js']
-        }
-    },
-    mocha_istanbul: {
-        coverage: {
-            src: 'spec'
-        },
+  jshint: {
+    all: ['src/**/*.js', '*.js', 'spec/**/*.js', '*.js'],
+    options: {
+      undef: true,
+      node: true,
+      globals: {
+        require: true,
+        module: true,
+        setTimeout: true,
+        __dirname: true,
+        process: true,
+        setInterval: true,
+        /* MOCHA */
+        it: true,
+        describe: true,
+        before: true,
+        beforeEach: true,
+        after: true,
+        afterEach: true
+      }
     }
+  },
+  mochaTest: {
+    test: {
+      options: {
+        reporter: 'spec',
+        //captureFile: 'results.txt', // Optionally capture the reporter output to a file
+        quiet: false, // Optionally suppress output to standard out (defaults to false)
+        clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+      },
+      src: ['spec/**/*.js']
+    }
+  },
+  mocha_istanbul: {
+    coverage: {
+      src: 'spec'
+    }
+  },
+  watch: {
+    scripts: {
+      files: ['src/*.js', 'spec/*.js'],
+      tasks: ['test'],
+      options: {
+        spawn: false
+      }
+    }
+  }
 });
 
 // Register tasks.
@@ -46,6 +55,7 @@ grunt.initConfig({
 grunt.loadNpmTasks('grunt-mocha-test');
 grunt.loadNpmTasks('grunt-contrib-jshint');
 grunt.loadNpmTasks('grunt-mocha-istanbul');
+grunt.loadNpmTasks('grunt-contrib-watch');
 
-grunt.registerTask('test', ['jshint:all', 'mocha_istanbul:coverage']);
+grunt.registerTask('test', ['jshint:all', 'mochaTest', 'mocha_istanbul:coverage', 'watch']);
 grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
