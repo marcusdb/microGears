fa5c09ca3ba16a11dd750d5bf16c7ebd47586e58
marcus <marcusdb@gmail.com>
2016-04-23 10:48:00 -0300

    fixing typings

--- microgears.d.ts
+++ microgears.d.ts
@@ -2,33 +2,34 @@
 // Project: http://github.com/marcusdb/microgears
 // Definitions by: Marcus David Bronstein <https://github.com/marcusdb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-declare namespace microgears {
-    export interface Service {
-        name:string;
-        async?:boolean;
-        pathname?:string;
-        namespace:string;
-    }
-
-    export interface MetaInformation {
-        serviceName:string;
-        methodName:string;
-        serviceNameSpace:string;
-        extra:any;
-    }
-
-    export interface Plugin {
-        name:string;
-        beforeChain<M extends MetaInformation>(arguments:Array<any>, metaInfo:M):Array<any>;
-        afterChain<T,M extends MetaInformation>(result:T, metaInfo:M):T;
-    }
-
-    export function addService<T extends Service>(service:T):T;
-
-    export function addPlugin<T extends Plugin>(plugin:T):void;
-}
-
 declare module "microgears" {
+
+    namespace microgears {
+        export interface Service {
+            name: string;
+            async ? : boolean;
+            pathname ? : string;
+            namespace: string;
+        }
+
+        export interface MetaInformation {
+            serviceName: string;
+            methodName: string;
+            serviceNameSpace: string;
+            extra: any;
+        }
+
+        export interface Plugin {
+            name: string;
+            beforeChain < M extends MetaInformation > (arguments: Array < any > , metaInfo: M): Array < any > ;
+            afterChain < T, M extends MetaInformation > (result: T, metaInfo: M): T;
+        }
+
+        export function addService < T extends Service > (service: T): T;
+
+        export function addPlugin < T extends Plugin > (plugin: T): void;
+    }
+
+
     export = microgears;
 }
