e731ef1f8ae7c73f7e2dc00e45793de428820c52
marcus <marcusdb@gmail.com>
2016-04-23 11:13:06 -0300

    typings adjusting still

--- microgears.d.ts
+++ microgears.d.ts
@@ -1,35 +1,31 @@
-// Type definitions for microgears v4.0.0
+// Type definitions for microgears v4.0.x
 // Project: http://github.com/marcusdb/microgears
 // Definitions by: Marcus David Bronstein <https://github.com/marcusdb>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-declare module "microgears" {
-
-    namespace microgears {
-        export interface Service {
-            name: string;
-            async ? : boolean;
-            pathname ? : string;
-            namespace: string;
-        }
-
-        export interface MetaInformation {
-            serviceName: string;
-            methodName: string;
-            serviceNameSpace: string;
-            extra: any;
-        }
-
-        export interface Plugin {
-            name: string;
-            beforeChain < M extends MetaInformation > (arguments: Array < any > , metaInfo: M): Array < any > ;
-            afterChain < T, M extends MetaInformation > (result: T, metaInfo: M): T;
-        }
-
-        export function addService < T extends Service > (service: T): T;
-
-        export function addPlugin < T extends Plugin > (plugin: T): void;
-    }
-
-
-    export = microgears;
+export interface Service {
+  name:string;
+  async ?:boolean;
+  pathname ?:string;
+  namespace:string;
 }
+
+export interface MetaInformation {
+  serviceName:string;
+  methodName:string;
+  serviceNameSpace:string;
+  extra:any;
+}
+
+export interface Plugin {
+  name:string;
+  beforeChain < M extends MetaInformation > (arguments:Array < any >, metaInfo:M):Array < any > ;
+  afterChain < T, M extends MetaInformation > (result:T, metaInfo:M):T;
+}
+
+export function addService < T extends Service >(service:T):T;
+
+export function addPlugin < T extends Plugin >(plugin:T):void;
+
+
+
+
+
